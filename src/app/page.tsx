18f26fd67f594d3e11edad6ef3864662ef5e1095
Hieu475 <quochieu04072005@gmail.com>
2025-12-22 17:42:17 +0700

    03:authentication-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
-import { Button } from "../components/ui/button"
+"use client";
 
+import { useState } from "react";
+
+import {authClient} from "@/lib/auth-client";
+import { Button } from "../components/ui/button";
+import {Input} from "../components/ui/input";
 export default function Home() {
+  const { 
+        data: session, 
+    } = authClient.useSession() 
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password,
+    }, {
+      onError: () => {
+        window.alert("Something went wrong");
+      },
+      onSuccess: () => {
+        window.alert("Success");
+      }
+    }
+  
+  );
+  if (session){
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>
+          Logged in as {session.user.name}
+        </p>
+        <button onClick={() => authClient.signOut()}>
+          Sign out
+        </button>
+      </div>
+    )
+  }
+  }
   return (
-    <button>
-      Click me 
-    </button>
+    <div className="flex flex-col gap-y-10">
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Button onClick = {onLogin}>Login</Button>
+      </div>
+    </div>
   )
 }
